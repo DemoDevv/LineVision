408e33eeaff8aece8d20f9916fd1777aa646c131
DemoDevv <mathieulebras95@gmail.com>
2025-11-30 21:45:15 +0100

    fix: remove a memory leak and fix printing for each channel

--- src/image.c
+++ src/image.c
@@ -11,30 +11,29 @@ Image* open_image(const char* filename, int desired_channels) {
 
     int width, height, channels;
     unsigned char* data = stbi_load(filename, &width, &height, &channels, desired_channels);
-    if (!data)
+    if (!data) {
+        free(image);
         return NULL;
+    }
 
     image->width = width;
     image->height = height;
-    if (desired_channels > 0) {
-        image->channels = desired_channels;
-    } else {
-        image->channels = channels;
-    }
+    image->channels = (desired_channels > 0) ? desired_channels : channels;
     image->data = data;
 
     return image;
 }
 
 void close_image(Image* image) {
-    if (!image)
-        return;
+    if (!image) return;
 
     stbi_image_free(image->data);
     free(image);
 }
 
 void print_image_info(Image* image) {
+    if (!image) return;
+
     printf("Image Info:\n");
     printf("Width: %d\n", image->width);
     printf("Height: %d\n", image->height);
@@ -42,9 +41,13 @@ void print_image_info(Image* image) {
 }
 
 void print_image(Image* image) {
+    if (!image) return;
+
     for (int y = 0; y < image->height; y++) {
         for (int x = 0; x < image->width; x++) {
-            printf("%u ", image->data[y * image->width * image->channels + x * image->channels]);
+            for (int c = 0; c < image->channels; c++) {
+                printf("%u ", image->data[y * image->width * image->channels + x * image->channels + c]);
+            }
         }
         printf("\n");
     }
