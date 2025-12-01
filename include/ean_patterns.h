a039c4d46a0ef1ec2fa8dee5a8df274fa551b4fe
DemoDevv <mathieulebras95@gmail.com>
2025-12-01 17:49:09 +0100

    feat: add pattern table for Set B of EAN code

--- include/ean_patterns.h
+++ include/ean_patterns.h
@@ -1,3 +1,4 @@
 #pragma once
 
 extern const int L_CODE[10];
+extern const int R_CODE[10];
--- src/ean_patterns.c
+++ src/ean_patterns.c
@@ -1,5 +1,6 @@
 #include "ean_patterns.h"
 
+// Coding Table for EAN-8 (Set A)
 const int L_CODE[10] = {
   0b0001101,
   0b0011001,
@@ -11,4 +12,18 @@ const int L_CODE[10] = {
   0b0111011,
   0b0110111,
   0b0001011,
+};
+
+// Coding Table for EAN-8 (Set B)
+const int R_CODE[10] = {
+  0b1110010,
+  0b1100110,
+  0b1101100,
+  0b1000010,
+  0b1011100,
+  0b1001110,
+  0b1010000,
+  0b1000100,
+  0b1001000,
+  0b1110100,
 };
