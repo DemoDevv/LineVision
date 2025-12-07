9f602bc06447e0829deb28c7859218ee0dbf2301
DemoDevv <mathieulebras95@gmail.com>
2025-12-07 01:23:24 +0100

    fix: refactorisation of the find_module function

--- src/decode.c
+++ src/decode.c
@@ -1,34 +1,44 @@
 #include "decode.h"
 #include <stdlib.h>
 
-int find_module(const uint8_t* segment, int length) {
-    int* hist = calloc(length, sizeof(int));
+size_t find_module(const uint8_t* segment, int length) {
+    if (!segment || length <= 0) return 0;
+
+    int max_module_width = length / 10;
+    int* hist = calloc(max_module_width + 1 , sizeof(int));
+    if (!hist) return 0;
+
     int counter = 0;
-    int max = 0, max_value = 0;
+    uint8_t current_color = segment[0];
 
     for (int i = 0; i < length; i++) {
-        if (segment[i] == 255 && counter > 0) {
-            hist[counter]++;
-
-            if (hist[counter] > max) {
-                max = hist[counter];
-                max_value = counter;
-            }
-
-            counter = 0;
-        } else if (segment[i] == 0) {
+        if (segment[i] == current_color) {
             counter++;
+        } else {
+            // Transition detected
+            if (current_color == 0 && counter > 0 && counter <= max_module_width) {
+                hist[counter]++;
+            }
+            current_color = segment[i];
+            counter = 1;
         }
     }
 
-    if (counter > 0) {
+    if (current_color == 0 && counter > 0 && counter <= max_module_width) {
         hist[counter]++;
-        if (hist[counter] > max) {
-            max = hist[counter];
-            max_value = counter;
+    }
+
+    int max_count = 0;
+    size_t module_width = 1;
+
+    // find the max
+    for (int i = 1; i <= max_module_width; i++) {
+        if (hist[i] > max_count) {
+            max_count = hist[i];
+            module_width = i;
         }
     }
 
     free(hist);
-    return max_value;
+    return module_width;
 }
